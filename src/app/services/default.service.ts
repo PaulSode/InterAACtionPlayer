c01fa4f0648013246679e1d1cebe114478910c5f
Noars <55838787+Noars@users.noreply.github.com>
2022-09-08 15:22:10 +0200

    Add dynamical version + date

--- src/app/services/default.service.ts
+++ src/app/services/default.service.ts
@@ -9,20 +9,29 @@ import { ThemeService } from './theme.service';
 import { AlertService } from '../playlist/services/alert.service';
 import { PlaylistService } from '../playlist/services/playlist.service';
 import { UsersService } from './users.service';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DefaultService {
 
-  VERSION = "InteraactionPlayer v.2022.05.03";
+  VERSION = "";
 
   constructor(private dwellTimeService: DwelltimeService,
               private languageService: LanguageService,
               private themeService: ThemeService,
               private alertService: AlertService,
               private playlistService: PlaylistService,
-              private usersService: UsersService) {
+              private usersService: UsersService,
+              private http: HttpClient) {
+    this.setVersion();
+  }
+
+  setVersion(){
+    this.http.get("https://api.github.com/repos/AFSR/InteraactionPlayer-AFSR/releases/latest").subscribe(data => {
+      this.VERSION = data["name"] + " Dev v." + data["created_at"].substring(0, 10).replace("-", ".");
+    })
   }
 
   setToDefault(){
